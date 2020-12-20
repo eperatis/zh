c9b8ba38c116fa92b96d24afcb9002c9546aa358
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-12-20 11:14:00 +0100

    Update api.js
    Change hungarian comments to english

--- src/api.js
+++ src/api.js
@@ -1,36 +1,36 @@
 var celZero = -273.15;
 var fahZero = -459.67;
 
-//fahrenheitből kelvinbe
+//from fahrenheit to kelvin
 const fahToKel = (input, resolve, reject) => {
     let result = (input-32)/1.8 + celZero;
-    result >= 0 ? resolve(result) : reject(`Nem megfelelő input`);
+    result >= 0 ? resolve(result) : reject('Invalid input');
 }
 
-//kelvinből fahrenheitbe
+//from kelvin to fahrenheit
 const kelToFah = (input, resolve, reject) => {
-    input >= 0 ? resolve(((input-celZero)*1.8)+32) : reject('Nem megfelelő input');
+    input >= 0 ? resolve(((input-celZero)*1.8)+32) : reject('Invalid input');
 }
 
-//celsiusból fahrenheitbe
+//from celsius to fahrenheit
 const celToFah = (input, resolve, reject) => {
-    input >= celZero ? resolve((input*1.8)+32) : reject('Nem megfelelő input');
+    input >= celZero ? resolve((input*1.8)+32) : reject('Invalid input');
 }
 
-//fahrenheitből celsiusba
+//from fahrenheit to celsius
 const fahToCel =  (input, resolve, reject) => {
-    input >= fahZero ? resolve((input-32)/1.8) : reject('Nem megfelelő input!');
+    input >= fahZero ? resolve((input-32)/1.8) : reject('Invalid input');
 }
 
-//celsiusból kelvibe
+//from celsius to kelvin
 const celToKel = (input, resolve, reject) => {
     let result = input - celZero;
-    result < 0 ? resolve(result) : reject("Nem megfelelő input!");
+    result < 0 ? resolve(result) : reject('Invalid input');
 }
 
-//kelvinből celsiusba
+//from kelvin to celsius
 const kelToCel = (input, resolve, reject) => {
-    input >= 0 ? resolve(input + celZero) : reject('Nem megfelelő input!');
+    input >= 0 ? resolve(input + celZero) : reject('Invalid input');
 }
 
 module.exports = {
